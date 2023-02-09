19628533e6c8722cd4a777fa0c497a63fcef3008
recorror <itgwd9701@gamil.com>
2023-02-09 16:16:54 +0900

    Modify : Home 캘린더
    - 이것저것 실험결과 그냥 이미지 여러개 쓰는 게 나을듯!

--- front_nanal_web/tailwind.config.js
+++ front_nanal_web/tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'sketch-book': 'src/src_assets/img/diaryImg.svg',
+      }
+    },
   },
   plugins: [],
 }
